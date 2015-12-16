6e9ef5a7bc34cc563751b6e64c4f5343ec1bc61b
Baby Poof <71001027@H-45180.sacs.k12.in.us>
2015-12-16 08:59:51 -0500

    Changed player who will be silent at first but strike if they lose to SilentButDeadly

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -145,7 +145,7 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #only betrays if they were a sucker last round.
     elif player == 2:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'SilentButDeadly'
         else:
             if len(opponent_history)==0: #It's the first round: collude
                 return 'c'
